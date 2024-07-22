c604780bd0b413561889b4adecc90e4b56679ff6
Jakub Jaworski <jawor182@proton.me>
2024-07-22 20:41:49 +0200

    Init

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    alias v="vim"
+    alias n="nvim"
+    alias b="btop"
+    alias z="zellij"
+end
