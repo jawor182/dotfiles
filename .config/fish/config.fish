e0807d0b611bd2926071b5e6cdf83d7b9aa5380e
jawor182 <kubajaworski59@outlook.com>
2024-08-01 20:38:26 +0200

    Remove tmux - shite

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ if status is-interactive
     alias b="btop"
     alias z="zellij"
     alias y="yazi"
+    alias t="tmux"
     alias update="sudo pacman -Syu && yay -Syu"
     starship init fish | source
     # fzf
