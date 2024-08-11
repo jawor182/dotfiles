9bea2dea772d046339fa6d770467062279073d47
jawor182 <kubajaworski59@outlook.com>
2024-08-11 19:56:50 +0200

    Nvim more

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
 require("config.keymap")
 require("config.opts")
+---@diagnostic disable-next-line: different-requires
 require("lazy").setup("plugins")
--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,10 +1,10 @@
-local harpoon = require("harpoon")
 
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>tr", vim.cmd.Ex)
 vim.keymap.set("n",'K', vim.lsp.buf.hover, {})
 
+local harpoon = require("harpoon")
 
 harpoon:setup()
 
@@ -31,7 +31,7 @@ vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_w
 vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
 vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
