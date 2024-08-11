4738ae8acd76ea437794c0f79ad232f780148d13
jawor182 <kubajaworski59@outlook.com>
2024-08-11 20:11:39 +0200

    Nvim more

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -35,3 +35,5 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
+
+vim.keymap.set('n','<leader>gf', vim.lsp.buf.format, {})
