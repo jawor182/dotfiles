a4e991eebc327786eb4dd2cb3cbbd95225ba87a5
jawor182 <kubajaworski59@outlook.com>
2024-08-14 17:21:28 +0200

    PHP + SQL

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,23 +1,32 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.completion.spell,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.diagnostics.golangci_lint,
-				null_ls.builtins.diagnostics.markdownlint,
-				null_ls.builtins.diagnostics.pylint,
-				null_ls.builtins.diagnostics.sqlfluff,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.completion.spell,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.diagnostics.golangci_lint,
+                null_ls.builtins.diagnostics.markdownlint,
+                null_ls.builtins.diagnostics.pylint,
+                null_ls.builtins.diagnostics.sqlfluff,
                 null_ls.builtins.formatting.clang_format,
-			},
-		})
-	end,
+                null_ls.builtins.formatting.pint,
+                null_ls.builtins.formatting.phpcbf,
+                null_ls.builtins.diagnostics.phpstan,
+                null_ls.builtins.diagnostics.sqlfluff.with({
+                    extra_args = { "--dialect", "sqlite" }, -- change to your dialect
+                }),
+                null_ls.builtins.formatting.sqlfluff.with({
+                    extra_args = { "--dialect", "sqlite" }, -- change to your dialect
+                }),
+            },
+        })
+    end,
 }
