26e6763187983b7d2683da3289f06bd068893dfa
jawor <jakubjaworski@mailbox.org>
2025-12-14 15:57:37 +0100

    Tweaks to nvim tmux and wallpaper script

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -11,6 +11,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-q>', '<C-w>q', { noremap = true, silent = true })
 
 local harpoon = require("harpoon")
 harpoon:setup()
