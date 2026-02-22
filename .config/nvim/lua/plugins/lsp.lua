7790184e2c5ab831d22b1de65fd0aceb89989fbd
jawor <jakubjaworski@mailbox.org>
2026-02-22 13:41:45 +0100

    Fuck obsidian.lua + small tweaks

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -51,7 +51,7 @@ return {
                 "ts_ls",
                 "tailwindcss",
                 "yamlls",
-                "markdown_oxide",
+                "marksman",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
