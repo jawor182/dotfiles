5fd0f97c67457533a9611e0471115ea93f4f87a6
jawor <jakubjaworski@mailbox.org>
2026-03-03 22:34:06 +0100

    tweaks

--- .config/nvim/lua/config/netrw.lua
+++ .config/nvim/lua/config/netrw.lua
@@ -1,7 +1,7 @@
 vim.g.netrw_liststyle = 0
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
-vim.g.netrw_keepdir = 0
+vim.g.netrw_keepdir = 1
 vim.g.netrw_chgperm = 1
 vim.g.netrw_hide = 1
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
