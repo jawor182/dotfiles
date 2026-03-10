9f2c62403f9441f3574e6c695a34ce1ba78f7e5a
jawor <jakubjaworski@mailbox.org>
2026-03-10 15:09:34 +0100

    Added back legacy treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "lua", "php", "javascript", "html", "css", "tsx", "typescript" },
+            sync_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            auto_install = true,
+        })
+    end,
 }
