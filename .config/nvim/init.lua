c158d4212258ccc3fb4fe7cbf240f88f2b1a1176
jawor <jakubjaworski@mailbox.org>
2026-06-26 23:09:05 +0200

    nvim indent fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,7 +62,6 @@ vim.pack.add({
 vim.api.nvim_create_autocmd('FileType', {
     callback = function()
         pcall(vim.treesitter.start)
-        vim.bo.indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
     end
 })
 
